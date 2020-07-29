7dbe70c93afe93a14dd3f8b571083779e47fbe93
LeMecAvecLaCageAHamster <quentinpenault@live.fr>
2020-07-29 11:16:43 +0200

    Feat/files upload (#3)
    * Add files input to upload configurations to back end api
    * Add files upload to nodejs API
    * Fix DeepScan issues
    * Add some Angular unit tests
    
    Co-authored-by: Quentin PÉNAULT <quentinpenault@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,25 +13,17 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        AppService,
+        HttpClient,
+        HttpHandler
+      ]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'EcoTree'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('EcoTree');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('EcoTree app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AppService } from './app.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private appService: AppService) { }
+
   title = 'EcoTree';
+
+  @Input() newMap: string = '';
+  @Input() files: string[] = [];
+
+  form = new FormGroup({
+    files: new FormControl('', [Validators.required])
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onFileChange(event) {
+    for (var i = 0; i < event.target.files.length; i++) {
+      this.files.push(event.target.files[i]);
+    }
+  }
+
+  // Upload files to back service
+  launchMrRobot() {
+    this.appService.uploadFiles(this.files);
+  }
 }
